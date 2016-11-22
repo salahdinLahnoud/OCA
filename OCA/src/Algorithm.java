2872e9bfe6b26f562c087519612cf982ac47d4b7
Salahdin Lahnoud <salahdin.lahnoud@gmail.com>
2016-11-22 12:45:20 +0100

    first change method print created..

---
+++ OCA/src/Algorithm.java
@@ -0,0 +1,32 @@
+
+public class Algorithm {
+	
+	public static void main(String [] args){
+		
+		
+		int [] array = {23, 4,5, 66, 12, 9, 0 , 34};
+		
+		print(array);
+		
+		
+	}
+	
+	
+	
+	public static void print(int [] array){
+		
+		
+		for(int i =0 ; i< array.length ; i++){
+			
+			
+			System.out.print(array[i]+ " , ");
+			
+			
+		}
+		
+		
+	}
+	
+	
+
+}
