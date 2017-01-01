4f07e6a5f71d1e431e29d5ea5b29d89585047e69
Salahdin Lahnoud <salahdin.lahnoud@gmail.com>
2017-01-01 21:27:49 +0100

    Test

---
+++ OCA/src/Test.java
@@ -0,0 +1,13 @@
+
+public class Test {
+
+	//varargs is dezelfde als array []
+	public static void main(String ... args){
+		
+		String name = "Salahdin";
+		String firstname ="Lahnoud";
+		
+		System.out.println(name + " " + firstname);		
+	}
+	
+}
