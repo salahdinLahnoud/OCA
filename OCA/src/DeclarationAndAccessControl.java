552600700045d18c1e190fc651e5325cdaf430fe
Salahdin Lahnoud <salahdin.lahnoud@gmail.com>
2017-01-02 17:05:57 +0100

    test identifier in java

---
+++ OCA/src/DeclarationAndAccessControl.java
@@ -0,0 +1,21 @@
+
+public class DeclarationAndAccessControl {
+	
+	//legal identifier...
+	int _a;
+	int $c;
+	int _____2_3_4_5_w;
+	
+	int _$;
+	int ident_ifier_salah;
+	
+	//illegal identifier...
+
+	//int :b;
+	//int -d;
+	//int e#;
+	//int .f;
+	//int 7g;
+	
+	
+}
